812b29f6ed8c7ce3244b0a02defd1a4d38c2e240
Luke Hackett <luke-hackett@outlook.com>
2022-12-18 23:42:20 +0000

    add debug mode, fixed route to specified one

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     cache_id = str(uuid4())
@@ -45,4 +45,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
