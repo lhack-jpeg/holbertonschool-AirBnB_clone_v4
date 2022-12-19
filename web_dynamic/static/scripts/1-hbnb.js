920d758d05104dd7875828b034667c3096b1d3a1
Luke Hackett <luke-hackett@outlook.com>
2022-12-19 15:30:33 +1100

    trying to call function after each click and logging.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,15 @@
 const checkDataDict = () => {
     const amenityList = [];
     let checkedInput = $("input:checked");
+    let checkInputLength = checkedInput.length;
     for (const item of checkedInput) {
         console.log({ item });
     }
+
     console.log({ checkedInput });
 };
+
+checkDataDict();
 $(function () {
     console.log("Page loaded");
     $("input[type=checkbox]").click(checkDataDict());
