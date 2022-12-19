395ffca57cd0d76ca8a3c4a7b2e7e8aefe13acef
Luke Hackett <luke-hackett@outlook.com>
2022-12-19 15:39:26 +1100

    remove status check

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -13,12 +13,5 @@ const checkDataDict = () => {
 checkDataDict();
 console.log({ amenity_list });
 $(function () {
-    $("input[type=checkbox]").click(checkDataDict());
+    $("input[type=checkbox]").click(checkDataDict);
 });
-
-$.get(
-    "http://f27b91ad72f7.cd567695.hbtn-cod.io:5001/api/v1/status/",
-    (data) => {
-        console.log({ data });
-    }
-);
