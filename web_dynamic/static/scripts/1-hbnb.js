921943f7bdeb46e4fc622920724654b8203bd9cb
Luke Hackett <luke-hackett@outlook.com>
2022-12-19 13:49:06 +1100

    add get request for api status

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,15 @@
-data_dict = {};
+const data_dict = {};
 const checkDataDict = () => {
     let checkedInput = $("input:checked");
     console.log({ checkedInput });
 };
 $(function () {
-    $("input[type=checkbox]").click(checkDataDict);
+    $("input[type=checkbox]").click(checkDataDict());
 });
+
+$.get(
+    "http://f27b91ad72f7.cd567695.hbtn-cod.io:5001/api/v1/status/",
+    (data) => {
+        console.log({ data });
+    }
+);
