1f6e816da159d7da891bfa734754526ca69e8699
Luke Hackett <luke-hackett@outlook.com>
2022-12-20 11:13:55 +1100

    moved function to inside load ready wrapper

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -27,6 +27,38 @@ $(function () {
             }
         }
     );
+    $("button").click(function () {
+        console.log(Object.keys(dictResults));
+        $.ajax({
+            type: "POST",
+            url: "http://f27b91ad72f7.cd567695.hbtn-cod.io:5001/api/v1/places_search/",
+            data: JSON.stringify({ amenities: Object.values(dictResults) }),
+            contentType: "application/json",
+            success: function (response) {
+                $("section.places").empty();
+                for (const place of response) {
+                    $(`<article>
+                <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                <div class="max_guest">${place.max_guest}Guest${
+                        place.max_guest !== 1 ? "s" : ""
+                    }</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${
+                        place.number_rooms !== 1 ? "s" : ""
+                    }</div>
+                <div class="number_bathrooms">${
+                    place.number_bathrooms
+                } Bathroom${place.number_bathrooms !== 1 ? "s" : ""}</div>
+                </div>
+                <div class="description">${place.description}</div>
+                </article>`).appendTo("section.places");
+                }
+            },
+        });
+    });
 });
 
 $.ajax({
@@ -69,30 +101,3 @@ $.ajax({
         });
     },
 });
-
-$('button').click(function () {
-    console.log(Object.keys(dictResults));
-      $.ajax({
-        type: 'POST',
-        url: 'http://f27b91ad72f7.cd567695.hbtn-cod.io:5001/api/v1/places_search/',
-        data: JSON.stringify({amenities: Object.values(dictResults)}),
-        contentType: 'application/json',
-        success: function (response) {
-          $('section.places').empty();
-          for (const place of response) {
-            $(`<article>
-            <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">${place.price_by_night}</div>
-            </div>
-            <div class="information">
-            <div class="max_guest">${place.max_guest}Guest${place.max_guest !== 1 ? 's' : ''}</div>
-            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-            </div>
-            <div class="description">${place.description}</div>
-            </article>`).appendTo('section.places');
-          }
-        }
-      });
-  });
