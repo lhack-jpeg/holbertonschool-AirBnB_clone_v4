6bd85f12a7772095e57c2ac13718080add365f47
Luke Hackett <luke-hackett@outlook.com>
2022-12-20 11:45:02 +1100

    change to stringify

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -33,7 +33,7 @@ $(function () {
             "http://f27b91ad72f7.cd567695.hbtn-cod.io:5001/api/v1/places_search/",
             {
                 data: JSON.stringify({}),
-                amenities: JSON.parse(selectedAmenity),
+                amenities: JSON.stringify(selectedAmenity),
                 contentType: "application/json",
             },
             function (response) {
