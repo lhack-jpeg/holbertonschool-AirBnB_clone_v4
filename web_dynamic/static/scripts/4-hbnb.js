08af074224d1f88463dc8d3d9fc43d8fef3cd16b
Luke Hackett <luke-hackett@outlook.com>
2022-12-20 11:42:57 +1100

    remove json from datatype

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -33,7 +33,6 @@ $(function () {
             "http://f27b91ad72f7.cd567695.hbtn-cod.io:5001/api/v1/places_search/",
             {
                 data: JSON.stringify({}),
-                dataType: "json",
                 amenities: selectedAmenity,
                 contentType: "application/json",
             },
